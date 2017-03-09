c8cd9f7fd54c8233daac60480838ae1bd530721b
korelstar <korelstar@users.noreply.github.com>
2017-03-09 16:50:29 +0100

    fix tests

--- app/src/androidTest/java/it/niedermann/owncloud/notes/model/NoteTest.java
+++ app/src/androidTest/java/it/niedermann/owncloud/notes/model/NoteTest.java
@@ -1,6 +1,11 @@
 package it.niedermann.owncloud.notes.model;
 
+import android.support.test.runner.AndroidJUnit4;
+
 import junit.framework.TestCase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.util.Calendar;
 
@@ -8,8 +13,10 @@ import java.util.Calendar;
  * Tests the Note Model
  * Created by stefan on 06.10.15.
  */
+@RunWith(AndroidJUnit4.class)
 public class NoteTest extends TestCase {
 
+    @Test
     public void testMarkDownStrip() {
         CloudNote note = new CloudNote(0, Calendar.getInstance(), "#Title", "", false, null, null);
         assertTrue("Title".equals(note.getTitle()));
